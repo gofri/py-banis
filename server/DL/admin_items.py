037dac55c1cb48c063de9c3a590b82f666c7caef
Gal Ofri <gofri@infinidat.com>
2021-08-03 20:29:47 +0300

    fixup bug from prev commit & add README

--- server/DL/admin_items.py
+++ server/DL/admin_items.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3.6
 # encoding: utf-8
-from . import common
+from . import database
 
 COLLECTION_KEY = 'ADMIN_ITEMS'
 
 def fetch_items():
-    data = common.DB.ins().get_collection(COLLECTION_KEY).find()
+    data = database.DB.ins().get_collection(COLLECTION_KEY).find()
     return data
 
 def put_items(items):
-    data = common.DB.ins().insert(COLLECTION_KEY, 'he', 'lo')
+    data = database.DB.ins().insert(COLLECTION_KEY, 'he', 'lo')
     return data
--- server/DL/users.py
+++ server/DL/users.py
@@ -1,7 +1,7 @@
  #!/usr/bin/python3.6
 # encoding: utf-8
 import flask
-from DL.common import DB
+from DL.database import DB
 
 class UsersDL(object):
     def __init__(self):
